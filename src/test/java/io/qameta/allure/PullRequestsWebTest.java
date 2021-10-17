7fbf1c157360eb3a06165a3a06801df6875ade8c
raik.tatyana <raik.tatyana@gmail.com>
2021-10-17 18:50:46 +0300

    upd

--- src/test/java/io/qameta/allure/PullRequestsWebTest.java
+++ src/test/java/io/qameta/allure/PullRequestsWebTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test;
  * @author eroshenkoam (Artem Eroshenko).
  */
 @Layer("web")
-@Owner("rike")
+@Owner("atam")
 @Feature("Pull Requests")
 public class PullRequestsWebTest {
 
